83006e2a7950f537c26d8e4aab984ba59063b763
Irvin Cardozo <64157096+irvinmarceloc@users.noreply.github.com>
2023-09-20 11:12:15 -0400

    Create auths.sh

---
+++ auths.sh
@@ -0,0 +1,30 @@
+NOMBRE_QM={{nombre_gestor_colas}}
+IP_FLOTANTE={{ip_flotante}}
+PUERTO_QM={{puerto}}
+INTERFAZ={{interfaz}}
+TAMANO_QM={{tam_m}}M
+NOMBRE_CANAL={{canal_de_conexion}}
+USUARIO_ADMINISTRADOR={{usuario}}
+
+# Crear el canal
+echo "DEFINE CHANNEL($NOMBRE_CANAL) CHLTYPE(SVRCONN) TRPTYPE(TCP)" | runmqsc $NOMBRE_QM
+
+# Estos mandatos otorgan a grupo '$USUARIO_ADMINISTRADOR' acceso administrativo completo en IBM MQ para UNIX y Linux.
+/opt/mqm/bin/setmqaut -m $NOMBRE_QM -t qmgr -g "$USUARIO_ADMINISTRADOR" +connect +inq +alladm
+/opt/mqm/bin/setmqaut -m $NOMBRE_QM -n "**" -t q -g "$USUARIO_ADMINISTRADOR" +alladm +crt +browse
+/opt/mqm/bin/setmqaut -m $NOMBRE_QM -n "**" -t topic -g "$USUARIO_ADMINISTRADOR" +alladm +crt
+/opt/mqm/bin/setmqaut -m $NOMBRE_QM -n "**" -t channel -g "$USUARIO_ADMINISTRADOR" +alladm +crt
+/opt/mqm/bin/setmqaut -m $NOMBRE_QM -n "**" -t process -g "$USUARIO_ADMINISTRADOR" +alladm +crt
+/opt/mqm/bin/setmqaut -m $NOMBRE_QM -n "**" -t namelist -g "$USUARIO_ADMINISTRADOR" +alladm +crt
+/opt/mqm/bin/setmqaut -m $NOMBRE_QM -n "**" -t authinfo -g "$USUARIO_ADMINISTRADOR" +alladm +crt
+/opt/mqm/bin/setmqaut -m $NOMBRE_QM -n "**" -t clntconn -g "$USUARIO_ADMINISTRADOR" +alladm +crt
+/opt/mqm/bin/setmqaut -m $NOMBRE_QM -n "**" -t listener -g "$USUARIO_ADMINISTRADOR" +alladm +crt
+/opt/mqm/bin/setmqaut -m $NOMBRE_QM -n "**" -t service -g "$USUARIO_ADMINISTRADOR" +alladm +crt
+/opt/mqm/bin/setmqaut -m $NOMBRE_QM -n "**" -t comminfo -g "$USUARIO_ADMINISTRADOR" +alladm +crt
+
+# Los siguientes mandatos proporcionan acceso administrativo para MQ Explorer.
+/opt/mqm/bin/setmqaut -m $NOMBRE_QM -n SYSTEM.MQEXPLORER.REPLY.MODEL -t q -g "$USUARIO_ADMINISTRADOR" +dsp +inq +get
+/opt/mqm/bin/setmqaut -m $NOMBRE_QM -n SYSTEM.ADMIN.COMMAND.QUEUE -t q -g "$USUARIO_ADMINISTRADOR" +dsp +inq +put
+
+# Actualizar políticas de seguridad
+echo "REFRESH SECURITY(*)" | runmqsc $NOMBRE_QM
